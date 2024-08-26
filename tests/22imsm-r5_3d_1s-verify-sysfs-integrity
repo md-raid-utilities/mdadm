md-raid-utilities/mdadm

commit 4dde420fc3e24077ab926f79674eaae1b71de10b

mdadm: remove container_enough logic
Arrays without enough disk count will be assembled but not
started.
Now RAIDs will be assembled always (even if they are failed).
RAID devices in all states will be assembled and exposed
to mdstat.
This change affects only IMSM (for ddf it wasn't used,
container_enough was set to true always).
Removed this logic from incremental_container as well with
runstop checking because runstop condition is being verified
in assemble_container_content function.

Signed-off-by: Pawel Piatkowski <pawel.piatkowski@intel.com>
Signed-off-by: Jes Sorensen <jes@trained-monkey.org>

Incremental.c
@@ -1467,17 +1467,6 @@ static int Incremental_container(struct supertype *st, char *devname,
 
 	st->ss->getinfo_super(st, &info, NULL);
 
-	if ((c->runstop > 0 && info.container_enough >= 0) ||
-	    info.container_enough > 0)
-		/* pass */;
-	else {
-		if (c->export) {
-			printf("MD_STARTED=no\n");
-		} else if (c->verbose)
-			pr_err("not enough devices to start the container\n");
-		return 0;
-	}
-
 	match = conf_match(st, &info, devname, c->verbose, &rv);
 	if (match == NULL && rv == 2)
 		return rv;

mdadm.h
@@ -377,9 +377,6 @@ struct mdinfo {
 	int container_member; /* for assembling external-metatdata arrays
 			       * This is to be used internally by metadata
 			       * handler only */
-	int container_enough; /* flag external handlers can set to
-			       * indicate that subarrays have not enough (-1),
-			       * enough to start (0), or all expected disks (1) */
 	char		sys_name[32];
 	struct mdinfo *devs;
 	struct mdinfo *next;

super-ddf.c
@@ -1975,7 +1975,6 @@ static void getinfo_super_ddf(struct supertype *st, struct mdinfo *info, char *m
 	info->array.ctime	  = DECADE + __be32_to_cpu(*cptr);
 
 	info->array.chunk_size	  = 0;
-	info->container_enough	  = 1;
 
 	info->disk.major	  = 0;
 	info->disk.minor	  = 0;

super-intel.c
@@ -3806,7 +3806,6 @@ static void getinfo_super_imsm(struct supertype *st, struct mdinfo *info, char *
 	struct intel_super *super = st->sb;
 	struct imsm_disk *disk;
 	int map_disks = info->array.raid_disks;
-	int max_enough = -1;
 	int i;
 	struct imsm_super *mpb;
 
@@ -3848,12 +3847,9 @@ static void getinfo_super_imsm(struct supertype *st, struct mdinfo *info, char *
 
 	for (i = 0; i < mpb->num_raid_devs; i++) {
 		struct imsm_dev *dev = get_imsm_dev(super, i);
-		int failed, enough, j, missing = 0;
+		int j = 0;
 		struct imsm_map *map;
-		__u8 state;
 
-		failed = imsm_count_failed(super, dev, MAP_0);
-		state = imsm_check_degraded(super, dev, failed, MAP_0);
 		map = get_imsm_map(dev, MAP_0);
 
 		/* any newly missing disks?
@@ -3868,36 +3864,10 @@ static void getinfo_super_imsm(struct supertype *st, struct mdinfo *info, char *
 
 			if (!(ord & IMSM_ORD_REBUILD) &&
 			    get_imsm_missing(super, idx)) {
-				missing = 1;
 				break;
 			}
 		}
-
-		if (state == IMSM_T_STATE_FAILED)
-			enough = -1;
-		else if (state == IMSM_T_STATE_DEGRADED &&
-			 (state != map->map_state || missing))
-			enough = 0;
-		else /* we're normal, or already degraded */
-			enough = 1;
-		if (is_gen_migration(dev) && missing) {
-			/* during general migration we need all disks
-			 * that process is running on.
-			 * No new missing disk is allowed.
-			 */
-			max_enough = -1;
-			enough = -1;
-			/* no more checks necessary
-			 */
-			break;
-		}
-		/* in the missing/failed disk case check to see
-		 * if at least one array is runnable
-		 */
-		max_enough = max(max_enough, enough);
 	}
-	dprintf("enough: %d\n", max_enough);
-	info->container_enough = max_enough;
 
 	if (super->disks) {
 		__u32 reserved = imsm_reserved_sectors(super, super->disks);

commit db0e3dc90aabd532d7f8b084d45c8d3efc8f6557

test: add test for checking sysfs integrity
Commit 4dde420fc3e2 ("mdadm: remove container_enough logic") introduced
a bug, where array sysfs intro is obtained from first drive added and
never updated. This results in info mismatch if not "up to date" drive
is added first. Due to this issue, the commit was reverted.

Add test for checking if sysfs entries are correctly set according to
metadata.

Check if consistency_policy: ppl is respected.

Signed-off-by: Mateusz Kusiak <mateusz.kusiak@intel.com>

tests/22imsm-r5_3d_1s-verify-sysfs-integrity
@@ -1,0 +1,73 @@
+# Commit 4dde420fc3e2 ("mdadm: remove container_enough logic") introduced a bug,
+# where array sysfs info is obtained from first drive added and never updated.
+# This results in info mismatch if not "up to date" drive is added first.
+
+# We are testing ppl here as:
+# - we intend to show the problem exists
+# - it's a simple scenario without overcomplicating things
+. tests/env-imsm-template
+
+check_if_ppl() {
+       local vol=$1
+       sysfs_path=$(get_sysdir $vol)
+       result=$(cat $sysfs_path/consistency_policy)
+       if [ "$result" -ne "ppl" ]
+       then
+              echo >&2 "Expected consistency_policy \"ppl\" but was \"$1\""
+              exit 1
+       fi
+       return 0
+}
+
+num_disks=4
+level=5
+chunk=64
+offset=0
+# Workaround for loopback
+size=$((size/2))
+
+# Create imsm RAID 5 with single spare
+mdadm --create $container --metadata=imsm --raid-disks=$num_disks $dev0 $dev1 $dev2 $dev3 --run
+imsm_check container $num_disks
+
+mdadm --create $member0 --level=$level --size=$size --chunk=$chunk \
+      --raid-disks=$((num_disks-1)) $dev0 $dev1 $dev2 --assume-clean --run
+udevadm settle
+imsm_check member $member0 $((num_disks-1)) $level $size $((size*(num_disks-2))) $offset $chunk
+
+# Write config file
+mdadm --examine --brief --scan > $config
+
+# Incremental fail single drive
+mdadm --incremental --fail $(basename $dev0)
+check wait
+imsm_check member $member0 $((num_disks-1)) $level $size $((size*(num_disks-2))) $offset $chunk
+testdev $member0 $((num_disks-2)) $size $chunk
+
+# Stop volume
+mdadm --stop $member0
+
+# Change consistency_policy to PPL
+mdadm --update-subarray=0 --update=ppl $container
+
+# Assemble volume
+mdadm --assemble $member0 --config=$config --verbose
+imsm_check member $member0 $((num_disks-1)) $level $size $((size*(num_disks-2))) $offset $chunk
+testdev $member0 $((num_disks-2)) $size $chunk
+
+# Check if consistency_policy is "ppl"
+check_if_ppl $member0
+
+# Perform stop scan
+mdadm --stop --scan
+
+# Incrementally assemble array, start from failed drive
+for dev in $dev0 $dev1 $dev2 $dev3
+do
+       mdadm --incremental $dev --config=$config
+done
+imsm_check member $member0 $((num_disks-1)) $level $size $((size*(num_disks-2))) $offset $chunk
+testdev $member0 $((num_disks-2)) $size $chunk
+
+# Check if consistency_policy is "ppl"
+check_if_ppl $member0

tests/env-ddf-template
@@ -11,12 +11,6 @@ get_rootdev() {
     echo $bd
 }
 
-get_sysdir() {
-    local mddev=$1
-    [ -L $mddev ] && mddev=$(readlink -f $mddev)
-    echo "/sys/class/block/$(basename $mddev)/md"
-}
-
 get_raiddisks() {
     sysdir=$(get_sysdir "$1")
     for i in $(seq 0 $(($(cat $sysdir/raid_disks)-1))); do

tests/func.sh
@@ -441,3 +441,9 @@ rotest() {
 	dev=$1
 	fsck -fn $dev >&2
 }
+
+get_sysdir() {
+    local mddev=$1
+    [ -L $mddev ] && mddev=$(readlink -f $mddev)
+    echo "/sys/class/block/$(basename $mddev)/md"
+}