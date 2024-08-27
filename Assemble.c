md-raid-utilities/mdadm

commit 4dde420fc3e24077ab926f79674eaae1b71de10b

mdadm: remove container_enough logic
Arrays without enough disk count will be assembled but not
started.
Now RAIDs will be assembled always (even if they are failed).
RAID devices in all states will be assembled and exposed
to mdstat.
This change affects only IMSM (for ddf it wasn't used,
container_enough was set to true always).
Removed this logic from incremental_container as well with
runstop checking because runstop condition is being verified
in assemble_container_content function.

Signed-off-by: Pawel Piatkowski <pawel.piatkowski@intel.com>
Signed-off-by: Jes Sorensen <jes@trained-monkey.org>

mdadm.h
@@ -377,9 +377,6 @@ struct mdinfo {
 	int container_member; /* for assembling external-metatdata arrays
 			       * This is to be used internally by metadata
 			       * handler only */
-	int container_enough; /* flag external handlers can set to
-			       * indicate that subarrays have not enough (-1),
-			       * enough to start (0), or all expected disks (1) */
 	char		sys_name[32];
 	struct mdinfo *devs;
 	struct mdinfo *next;

super-intel.c
@@ -3806,7 +3806,6 @@ static void getinfo_super_imsm(struct supertype *st, struct mdinfo *info, char *
 	struct intel_super *super = st->sb;
 	struct imsm_disk *disk;
 	int map_disks = info->array.raid_disks;
-	int max_enough = -1;
 	int i;
 	struct imsm_super *mpb;
 
@@ -3848,12 +3847,9 @@ static void getinfo_super_imsm(struct supertype *st, struct mdinfo *info, char *
 
 	for (i = 0; i < mpb->num_raid_devs; i++) {
 		struct imsm_dev *dev = get_imsm_dev(super, i);
-		int failed, enough, j, missing = 0;
+		int j = 0;
 		struct imsm_map *map;
-		__u8 state;
 
-		failed = imsm_count_failed(super, dev, MAP_0);
-		state = imsm_check_degraded(super, dev, failed, MAP_0);
 		map = get_imsm_map(dev, MAP_0);
 
 		/* any newly missing disks?
@@ -3868,36 +3864,10 @@ static void getinfo_super_imsm(struct supertype *st, struct mdinfo *info, char *
 
 			if (!(ord & IMSM_ORD_REBUILD) &&
 			    get_imsm_missing(super, idx)) {
-				missing = 1;
 				break;
 			}
 		}
-
-		if (state == IMSM_T_STATE_FAILED)
-			enough = -1;
-		else if (state == IMSM_T_STATE_DEGRADED &&
-			 (state != map->map_state || missing))
-			enough = 0;
-		else /* we're normal, or already degraded */
-			enough = 1;
-		if (is_gen_migration(dev) && missing) {
-			/* during general migration we need all disks
-			 * that process is running on.
-			 * No new missing disk is allowed.
-			 */
-			max_enough = -1;
-			enough = -1;
-			/* no more checks necessary
-			 */
-			break;
-		}
-		/* in the missing/failed disk case check to see
-		 * if at least one array is runnable
-		 */
-		max_enough = max(max_enough, enough);
 	}
-	dprintf("enough: %d\n", max_enough);
-	info->container_enough = max_enough;
 
 	if (super->disks) {
 		__u32 reserved = imsm_reserved_sectors(super, super->disks);

Incremental.c
@@ -1467,17 +1467,6 @@ static int Incremental_container(struct supertype *st, char *devname,
 
 	st->ss->getinfo_super(st, &info, NULL);
 
-	if ((c->runstop > 0 && info.container_enough >= 0) ||
-	    info.container_enough > 0)
-		/* pass */;
-	else {
-		if (c->export) {
-			printf("MD_STARTED=no\n");
-		} else if (c->verbose)
-			pr_err("not enough devices to start the container\n");
-		return 0;
-	}
-
 	match = conf_match(st, &info, devname, c->verbose, &rv);
 	if (match == NULL && rv == 2)
 		return rv;

super-ddf.c
@@ -1975,7 +1975,6 @@ static void getinfo_super_ddf(struct supertype *st, struct mdinfo *info, char *m
 	info->array.ctime	  = DECADE + __be32_to_cpu(*cptr);
 
 	info->array.chunk_size	  = 0;
-	info->container_enough	  = 1;
 
 	info->disk.major	  = 0;
 	info->disk.minor	  = 0;

commit df651eb414c17d2379c705f91ae21c2436c21ff9

mdadm: Prepare for container_enough logic removal
Commit 4dde420fc3e2 ("mdadm: remove container_enough logic") introduced a
bug, where array sysfs info is obtained from first drive added and never
updated. This resulted in info mismatch if not "up to date" drive was
added first.

Second issue, related to rebuilds, was omitting drives during rebuild.
This allowed starting array with rebuild drives missing, resulting in
rebuild starting from beginning if such drives were (incrementally)
added after healthy members.

This commit prepares the code with necessary changes for said commit to
be safely reapplied.

Split sysfs_set_array() into three functions:
- sysfs_set_array_init() for initiating the array in sysfs.
  This function fills enough sysfs entries for array to be visible
  in mdstat.
- sysfs_update_raid_disks() for sysfs entries that must be updated with
  each drive.
- sysfs_set_array() that fills all necessary sysfs entries for array
  to prosper.

Fill all sysfs entries, only when enough drives are added,
assuming the metadata info should be up to date at this point.

Add missing_disks count and rebuild_started to mdinfo, to verify if
array was performing rebuild or is missing drives.
Verify if reassembled volume includes all drives, including rebuild
drives prior to starting array. If array was degraded prior to stop and
rebuild was not happening, start array degraded.

Use generic error codes.

Signed-off-by: Mateusz Kusiak <mateusz.kusiak@intel.com>

Assemble.c
@@ -1983,9 +1983,10 @@ int assemble_container_content(struct supertype *st, int mdfd,
 	int old_raid_disks;
 	int start_reshape;
 	char *avail;
-	int err;
-	int is_clean, all_disks;
-	bool is_raid456;
+	int err = 0;
+	int is_clean, all_disks, missing_disks;
+	bool is_raid456, need_rebuild;
+	char buf[SYSFS_MAX_BUF_SIZE] = {0};
 
 	if (sysfs_init(content, mdfd, NULL)) {
 		pr_err("Unable to initialize sysfs\n");
@@ -1998,14 +1999,33 @@ int assemble_container_content(struct supertype *st, int mdfd,
 		return 1;
 	}
 
-	/* Fill sysfs properties only if they are not set. Determine it by checking text_version
-	 * and ignoring special character on the first place.
+	if (sysfs_get_str(content, NULL, "array_state", buf, sizeof(buf)) == 0) {
+		pr_err("Failed to read array_state!");
+		sysfs_free(sra);
+		return 1;
+	}
+
+	/* Array is already started, nothing to do here */
+	if (strncmp(buf, "clear", 5) != 0 &&
+	    strncmp(buf, "inactive", 8) != 0) {
+		sysfs_free(sra);
+		return MDADM_STATUS_SUCCESS;
+	}
+
+	/*
+	 * Fill necessary sysfs properties for array only if not set.
+	 * Determine it by checking text_version and ignoring special character on the first place.
 	 */
 	if (strcmp(sra->text_version + 1, content->text_version + 1) != 0) {
-		if (sysfs_set_array(content) != 0) {
+		if (sysfs_init_array(content) != MDADM_STATUS_SUCCESS) {
+			pr_err("Failed initiating sysfs for the array!\n");
 			sysfs_free(sra);
 			return 1;
 		}
+	} else if (sysfs_update_raid_disks(content) != MDADM_STATUS_SUCCESS) {
+		/* reason already spewed */
+		sysfs_free(sra);
+		return 1;
 	}
 
 	/* There are two types of reshape: container wide or sub-array specific
@@ -2014,6 +2034,16 @@ int assemble_container_content(struct supertype *st, int mdfd,
 	start_reshape = (content->reshape_active &&
 			 !((content->reshape_active == CONTAINER_RESHAPE) &&
 			   (content->array.state & (1<<MD_SB_BLOCK_CONTAINER_RESHAPE))));
+
+	/*
+	 * We need to differentiate disks in need of resync and rebuild.
+	 * The problem here is that recovery_start and resync_start are a union,
+	 * thus we need additional factor to verify what action is needed.
+	 */
+	need_rebuild = (content->missing_disks > 0);
+
+	/* If rebuild was not previously started, we can start array degraded */
+	missing_disks = content->rebuild_started ? content->missing_disks : 0;
 
 	/* Block subarray here if it is under reshape now
 	 * Do not allow for any changes in this array
@@ -2039,8 +2069,16 @@ int assemble_container_content(struct supertype *st, int mdfd,
 	old_raid_disks = content->array.raid_disks - content->delta_disks;
 	avail = xcalloc(content->array.raid_disks, 1);
 	for (dev = content->devs; dev; dev = dev->next) {
-		if (dev->disk.raid_disk >= 0)
-			avail[dev->disk.raid_disk] = 1;
+		if (dev->disk.raid_disk >= 0) {
+			/*
+			 * If rebuild is not needed count all drvies,
+			 * otherwise count healthy drives only.
+			 */
+			if (need_rebuild == false)
+				avail[dev->disk.raid_disk] = 1;
+			else if (dev->recovery_start == MaxSector)
+				avail[dev->disk.raid_disk] = 1;
+		}
 		if (sysfs_add_disk(content, dev, 1) == 0) {
 			if (dev->disk.raid_disk >= old_raid_disks &&
 			    content->reshape_active)
@@ -2130,12 +2168,22 @@ int assemble_container_content(struct supertype *st, int mdfd,
 	}
 	free(avail);
 
-	if (c->runstop <= 0 && all_disks < content->array.working_disks) {
+	/*
+	 * If enough() passed, necessary drives were added to achieve data integrity.
+	 * Without "--run" wait until all members are added back, including drives under rebuild.
+	 */
+	if (c->runstop <= 0 && (all_disks < content->array.working_disks + missing_disks)) {
 
 		set_array_assembly_status(c, result, INCR_UNSAFE, &array);
 
 		if (c->verbose >= 0 && c->force)
 			pr_err("Consider --run to start array as degraded.\n");
+		return 1;
+	}
+
+	/* If array passed previous checks, we assume it's up to date. Write sysfs. */
+	if (sysfs_set_array(content) != MDADM_STATUS_SUCCESS) {
+		pr_err("Failed writing sysfs array info!\n");
 		return 1;
 	}
 
@@ -2161,6 +2209,7 @@ int assemble_container_content(struct supertype *st, int mdfd,
 
 	if (start_reshape) {
 		int spare = content->array.raid_disks + array.exp_cnt;
+
 		if (restore_backup(st, content,
 				   array.new_cnt,
 				   spare, &c->backup_file, c->verbose) == 1)
@@ -2203,8 +2252,7 @@ int assemble_container_content(struct supertype *st, int mdfd,
 					    c->readonly ? "readonly" : "active");
 			break;
 		default:
-			err = sysfs_set_str(content, NULL, "array_state",
-					    "readonly");
+			err = sysfs_set_str(content, NULL, "array_state", "readonly");
 			/* start mdmon if needed. */
 			if (!err) {
 				if (!mdmon_running(st->container_devnm))

mdadm.h
@@ -377,6 +377,12 @@ struct mdinfo {
 	unsigned long long	ppl_sector;
 	unsigned long		safe_mode_delay; /* ms delay to mark clean */
 	int			new_level, delta_disks, new_layout, new_chunk;
+
+	/* Number of drives that needs rebuilding. */
+	int missing_disks;
+
+	/* Indicates that rebuild has not yet been started and is safe to start degraded array. */
+	bool			rebuild_started;
 	int			errors;
 	unsigned long		cache_size; /* size of raid456 stripe cache*/
 	int			mismatch_cnt;
@@ -448,6 +454,7 @@ typedef enum mdadm_status {
 	MDADM_STATUS_SUCCESS = 0,
 	MDADM_STATUS_ERROR,
 	MDADM_STATUS_UNDEF,
+	MDADM_STATUS_OPERATION_FAILED,
 } mdadm_status_t;
 
 enum mode {
@@ -831,6 +838,8 @@ extern int sysfs_attribute_available(struct mdinfo *sra, struct mdinfo *dev,
 extern int sysfs_get_str(struct mdinfo *sra, struct mdinfo *dev,
 			 char *name, char *val, int size);
 extern int sysfs_set_safemode(struct mdinfo *sra, unsigned long ms);
+extern int sysfs_init_array(struct mdinfo *info);
+int sysfs_update_raid_disks(struct mdinfo *info);
 extern int sysfs_set_array(struct mdinfo *info);
 extern int sysfs_add_disk(struct mdinfo *sra, struct mdinfo *sd, int resume);
 extern int sysfs_disk_to_scsi_id(int fd, __u32 *id);

super-intel.c
@@ -8190,15 +8190,17 @@ static struct mdinfo *container_content_imsm(struct supertype *st, char *subarra
 		for (slot = 0 ; slot <  map->num_members; slot++) {
 			unsigned long long recovery_start;
 			struct mdinfo *info_d;
-			struct dl *d;
-			int idx;
-			int skip;
-			__u32 ord;
+			__u32 ord, ord0;
 			int missing = 0;
+			struct dl *d;
+			int skip;
+			int idx;
 
 			skip = 0;
 			idx = get_imsm_disk_idx(dev, slot, MAP_0);
+			ord0 = get_imsm_ord_tbl_ent(dev, slot, MAP_0);
 			ord = get_imsm_ord_tbl_ent(dev, slot, MAP_X);
+
 			for (d = super->disks; d ; d = d->next)
 				if (d->index == idx)
 					break;
@@ -8208,10 +8210,20 @@ static struct mdinfo *container_content_imsm(struct supertype *st, char *subarra
 				skip = 1;
 			if (d && is_failed(&d->disk))
 				skip = 1;
-			if (!skip && (ord & IMSM_ORD_REBUILD))
-				recovery_start = 0;
-			if (!(ord & IMSM_ORD_REBUILD))
+			if (ord & IMSM_ORD_REBUILD) {
+				this->missing_disks++;
+
+				if (!skip)
+					recovery_start = 0;
+			} else {
 				this->array.working_disks++;
+			}
+
+			/* If rebuild started prior to stop, ord0 has no bit set, but ord has. */
+			if (!(ord0 & IMSM_ORD_REBUILD) &&
+			    get_imsm_missing(super, idx))
+				this->rebuild_started = true;
+
 			/*
 			 * if we skip some disks the array will be assmebled degraded;
 			 * reset resync start to avoid a dirty-degraded

sysfs.c
@@ -30,6 +30,10 @@
 
 #define MAX_SYSFS_PATH_LEN	120
 
+#define DCOLOR_RED	"\033[1;31m"
+#define DCOLOR_GREEN	"\033[1;32m"
+#define DCOLOR_DEFAULT	"\033[1;0m"
+
 struct dev_sysfs_rule {
 	struct dev_sysfs_rule *next;
 	char *devname;
@@ -490,17 +494,20 @@ int sysfs_set_str(struct mdinfo *sra, struct mdinfo *dev,
 	int fd;
 
 	snprintf(fname, MAX_SYSFS_PATH_LEN, "/sys/block/%s/md/%s/%s",
-		sra->sys_name, dev?dev->sys_name:"", name);
+		 sra->sys_name, dev ? dev->sys_name : "", name);
 	fd = open(fname, O_WRONLY);
 	if (fd < 0)
 		return -1;
+	dprintf("Writing \"%s\" to \"%s/%s\" (%s) : ",
+		val, dev ? dev->sys_name : "md", name, sra->sys_name);
 	n = write(fd, val, strlen(val));
 	close(fd);
 	if (n != strlen(val)) {
-		dprintf("failed to write '%s' to '%s' (%s)\n",
-			val, fname, strerror(errno));
+		dprintf_cont(DCOLOR_RED"failed!\n"DCOLOR_DEFAULT);
+		dprintf_cont("Reason: %s\n", strerror(errno));
 		return -1;
 	}
+	dprintf_cont(DCOLOR_GREEN"success!\n"DCOLOR_DEFAULT);
 	return 0;
 }
 
@@ -679,17 +686,25 @@ int sysfs_set_safemode(struct mdinfo *sra, unsigned long ms)
 	/*             this '\n' ^ needed for kernels older than 2.6.28 */
 	return sysfs_set_str(sra, NULL, "safe_mode_delay", delay);
 }
-
-int sysfs_set_array(struct mdinfo *info)
+/**
+ * sysfs_init_array() - Initiate array, set base sysfs entries for md-device to appear in mdstat.
+ *
+ * @info:	array info.
+ * Return:	MDADM_STATUS_SUCCESS if all writes succeeded,
+ *		MDADM_STATUS_OPERATION_FAILED if any write failed,
+ *
+ * For container status is MDADM_STATUS_SUCCESS, no actual writes are being done.
+ */
+int sysfs_init_array(struct mdinfo *info)
 {
 	int rv = 0;
-	char ver[100];
 	int raid_disks = info->array.raid_disks;
 
-	ver[0] = 0;
-	if (info->array.major_version == -1 &&
-	    info->array.minor_version == -2) {
-		char buf[SYSFS_MAX_BUF_SIZE];
+	dprintf("Performing necessary array sysfs writes.\n");
+
+	if (info->array.major_version == -1 && info->array.minor_version == -2) {
+		char buf[SYSFS_MAX_BUF_SIZE] = {0};
+		char ver[SYSFS_MAX_BUF_SIZE] = {0};
 
 		strcat(strcpy(ver, "external:"), info->text_version);
 
@@ -704,26 +719,89 @@ int sysfs_set_array(struct mdinfo *info)
 			if (strlen(buf) >= 9 && buf[9] == '-')
 				ver[9] = '-';
 
-		if (sysfs_set_str(info, NULL, "metadata_version", ver) < 0) {
-			pr_err("This kernel does not support external metadata.\n");
-			return 1;
-		}
+		rv |= sysfs_set_str(info, NULL, "metadata_version", ver);
 	}
-	if (info->array.level < 0)
-		return 0; /* FIXME */
-	rv |= sysfs_set_str(info, NULL, "level",
-			    map_num_s(pers, info->array.level));
+
+	if (is_container(info->array.level))
+		return MDADM_STATUS_SUCCESS;
+
+	rv |= sysfs_set_str(info, NULL, "level", map_num_s(pers, info->array.level));
+
+	/*
+	 * MD driver does not expose delta_disks via sysfs.
+	 * Delta disks are calculated based on raid_disks.
+	 * For this mechanism to work "old disks" number must be set first,
+	 * and then overwritten with new disk number.
+	 */
 	if (info->reshape_active && info->delta_disks != UnSet)
 		raid_disks -= info->delta_disks;
 	rv |= sysfs_set_num(info, NULL, "raid_disks", raid_disks);
+
+	dprintf("Done performing necessary array sysfs writes.\n");
+	if (rv != 0)
+		return MDADM_STATUS_OPERATION_FAILED;
+	return MDADM_STATUS_SUCCESS;
+}
+/**
+ * sysfs_update_raid_disks() - Updates raid_disks sysfs_entry as it's dynamically calculated.
+ *
+ * @info: array info.
+ * Return: MDADM_STATTUS_OPERATION_FAILED on read/write fail, MDADM_STATUS_SUCCESS otherwise.
+ */
+int sysfs_update_raid_disks(struct mdinfo *info)
+{
+	unsigned long long raid_disks = 0;
+
+	dprintf("Checking and updating sysfs for array.\n");
+
+	/* Read raid_disks value */
+	if (sysfs_get_ll(info, NULL, "raid_disks", &raid_disks) < 0) {
+		pr_err("Failed to read raid_disks!");
+		return MDADM_STATUS_OPERATION_FAILED;
+	}
+
+	/* Compare mdinfo to sysfs */
+	if ((unsigned long long) info->array.raid_disks > raid_disks)
+		if (sysfs_set_num(info, NULL, "raid_disks", info->array.raid_disks) != 0) {
+			pr_err("Failed to update raid_disks!");
+			return MDADM_STATUS_OPERATION_FAILED;
+		}
+
+	dprintf("Done updating sysfs.\n");
+
+	return MDADM_STATUS_SUCCESS;
+}
+
+/**
+ * sysfs_set_array() - Sets sysfs entries for the array.
+ *
+ * @info:	array info.
+ * Return:	MDADM_STATUS_SUCCESS if all writes succeeded,
+ *		MDADM_STATUS_OPERATION_FAILED if any write failed,
+ *		MDADM_STATUS_NATIVE_ONLY if kernel does not support external metadata,
+ *
+ * For container status is MDADM_STATUS_SUCCESS, no actual writes are being done.
+ */
+int sysfs_set_array(struct mdinfo *info)
+{
+	int rv = sysfs_init_array(info);
+
+	if (rv == MDADM_STATUS_OPERATION_FAILED)
+		return rv;
+
+	/* sysfs should not be set for a container */
+	if (is_container(info->array.level))
+		return MDADM_STATUS_SUCCESS;
+
+	dprintf("Performing extended sysfs array writes.\n");
+
 	rv |= sysfs_set_num(info, NULL, "chunk_size", info->array.chunk_size);
 	rv |= sysfs_set_num(info, NULL, "layout", info->array.layout);
 	rv |= sysfs_set_num(info, NULL, "component_size", info->component_size/2);
 	if (info->custom_array_size) {
 		int rc;
 
-		rc = sysfs_set_num(info, NULL, "array_size",
-				   info->custom_array_size/2);
+		rc = sysfs_set_num(info, NULL, "array_size", info->custom_array_size / 2);
 		if (rc && errno == ENOENT) {
 			pr_err("This kernel does not have the md/array_size attribute, the array may be larger than expected\n");
 			rc = 0;
@@ -735,20 +813,17 @@ int sysfs_set_array(struct mdinfo *info)
 		rv |= sysfs_set_num(info, NULL, "resync_start", info->resync_start);
 
 	if (info->reshape_active) {
-		rv |= sysfs_set_num(info, NULL, "reshape_position",
-				    info->reshape_progress);
+		rv |= sysfs_set_num(info, NULL, "reshape_position", info->reshape_progress);
 		rv |= sysfs_set_num(info, NULL, "chunk_size", info->new_chunk);
 		rv |= sysfs_set_num(info, NULL, "layout", info->new_layout);
-		rv |= sysfs_set_num(info, NULL, "raid_disks",
-				    info->array.raid_disks);
+		rv |= sysfs_set_num(info, NULL, "raid_disks", info->array.raid_disks);
 		/* We don't set 'new_level' here.  That can only happen
 		 * once the reshape completes.
 		 */
 	}
 
 	if (info->consistency_policy == CONSISTENCY_POLICY_PPL) {
-		char *policy = map_num_s(consistency_policies,
-					    info->consistency_policy);
+		char *policy = map_num_s(consistency_policies, info->consistency_policy);
 
 		if (sysfs_set_str(info, NULL, "consistency_policy", policy)) {
 			pr_err("This kernel does not support PPL. Falling back to consistency-policy=resync.\n");
@@ -756,7 +831,10 @@ int sysfs_set_array(struct mdinfo *info)
 		}
 	}
 
-	return rv;
+	dprintf("Done performing extended array sysfs writes.\n");
+	if (rv != 0)
+		return MDADM_STATUS_OPERATION_FAILED;
+	return MDADM_STATUS_SUCCESS;
 }
 
 int sysfs_add_disk(struct mdinfo *sra, struct mdinfo *sd, int resume)
@@ -767,13 +845,14 @@ int sysfs_add_disk(struct mdinfo *sra, struct mdinfo *sd, int resume)
 	int rv;
 	int i;
 
+	dprintf("(enter)\n");
 	sprintf(dv, "%d:%d", sd->disk.major, sd->disk.minor);
 	rv = sysfs_set_str(sra, NULL, "new_dev", dv);
 	if (rv)
 		return rv;
-
 	memset(nm, 0, sizeof(nm));
 	dname = devid2kname(makedev(sd->disk.major, sd->disk.minor));
+	dprintf("Performing sysfs writes for disk %s.\n", dname);
 	strcpy(sd->sys_name, "dev-");
 	strcpy(sd->sys_name+4, dname);
 
@@ -781,11 +860,12 @@ int sysfs_add_disk(struct mdinfo *sra, struct mdinfo *sd, int resume)
 	if (resume && sd->recovery_start < MaxSector &&
 	    sysfs_set_num(sra, sd, "recovery_start", 0)) {
 		sysfs_set_str(sra, sd, "state", "remove");
+		dprintf("Sysfs writes for disk %s failed!\n", dname);
 		return -1;
 	}
 
 	rv = sysfs_set_num(sra, sd, "offset", sd->data_offset);
-	rv |= sysfs_set_num(sra, sd, "size", (sd->component_size+1) / 2);
+	rv |= sysfs_set_num(sra, sd, "size", (sd->component_size + 1) / 2);
 	if (!is_container(sra->array.level)) {
 		if (sra->consistency_policy == CONSISTENCY_POLICY_PPL) {
 			rv |= sysfs_set_num(sra, sd, "ppl_sector", sd->ppl_sector);
@@ -823,6 +903,8 @@ int sysfs_add_disk(struct mdinfo *sra, struct mdinfo *sd, int resume)
 			rv |= sysfs_set_str(sra, sd, "bad_blocks", s);
 		}
 	}
+
+	dprintf("Done performing sysfs writes for disk %s.\n", dname);
 	return rv;
 }
 