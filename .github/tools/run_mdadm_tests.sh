db40ee31d9c5e4661dc8c79b9104891626618802
Kinga Stefaniuk <kinga.stefaniuk@intel.com>
2024-10-01 15:19:49 +0200

    CI: use self-hosted runner to run tests
    Use prepared VM machine in GitHub actions to run mdadm tests on it.
    
    Signed-off-by: Kinga Stefaniuk <kinga.stefaniuk@intel.com>

---
+++ .github/tools/run_mdadm_tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+sudo make clean
+sudo make -j$(nproc)
+sudo make install
+sudo dracut -f
+sudo mdadm -Ss
+sudo ./test setup
+sudo ./test --skip-broken --no-error --disable-integrity --disable-multipath --disable-linear --keep-going
+sudo ./test cleanup
