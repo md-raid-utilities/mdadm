md-raid-utilities/mdadm

commit 119cdcad049e0464bfdd9ab19e2fd8cb4f9a82d6

mdadm: drop auto= support
According to author (and what was described in man):
"With mdadm 3.0, device creation is normally left up to udev so this is
option is unlikely to be needed"
This was a workaround for kernel 2.6 family issues (partitionable and
non-partitionable arrays hell) and I believe we are far away from it now.

I'm not aware of any usage of it, hence it is removed.

Signed-off-by: Mariusz Tkaczyk <mariusz.tkaczyk@linux.intel.com>

mdopen.c
@@ -29,82 +29,6 @@
 
 #include <ctype.h>
 
-void make_parts(char *dev, int cnt)
-{
-	/* make 'cnt' partition devices for 'dev'
-	 * If dev is a device name we use the
-	 *  major/minor from dev and add 1..cnt
-	 * If it is a symlink, we make similar symlinks.
-	 * If dev ends with a digit, we add "p%d" else "%d"
-	 * If the name exists, we use it's owner/mode,
-	 * else that of dev
-	 */
-	struct stat stb;
-	int major_num;
-	int minor_num;
-	int odig;
-	int i;
-	int nlen = strlen(dev) + 20;
-	char *name;
-	int dig = isdigit(dev[strlen(dev)-1]);
-	char orig[1001];
-	char sym[1024];
-	int err;
-
-	if (cnt == 0)
-		cnt = 4;
-	if (lstat(dev, &stb)!= 0)
-		return;
-
-	if (S_ISBLK(stb.st_mode)) {
-		major_num = major(stb.st_rdev);
-		minor_num = minor(stb.st_rdev);
-		odig = -1;
-	} else if (S_ISLNK(stb.st_mode)) {
-		int len;
-
-		len = readlink(dev, orig, sizeof(orig));
-		if (len < 0 || len >= (int)sizeof(orig))
-			return;
-		orig[len] = 0;
-		odig = isdigit(orig[len-1]);
-		major_num = -1;
-		minor_num = -1;
-	} else
-		return;
-	name = xmalloc(nlen);
-	for (i = 1; i <= cnt ; i++) {
-		struct stat stb2;
-		snprintf(name, nlen, "%s%s%d", dev, dig?"p":"", i);
-		if (stat(name, &stb2) == 0) {
-			if (!S_ISBLK(stb2.st_mode) || !S_ISBLK(stb.st_mode))
-				continue;
-			if (stb2.st_rdev == makedev(major_num, minor_num+i))
-				continue;
-			unlink(name);
-		} else {
-			stb2 = stb;
-		}
-		if (S_ISBLK(stb.st_mode)) {
-			if (mknod(name, S_IFBLK | 0600,
-				  makedev(major_num, minor_num+i)))
-				perror("mknod");
-			if (chown(name, stb2.st_uid, stb2.st_gid))
-				perror("chown");
-			if (chmod(name, stb2.st_mode & 07777))
-				perror("chmod");
-			err = 0;
-		} else {
-			snprintf(sym, sizeof(sym), "%s%s%d", orig, odig?"p":"", i);
-			err = symlink(sym, name);
-		}
-
-		if (err == 0 && stat(name, &stb2) == 0)
-			add_dev(name, &stb2, 0, NULL);
-	}
-	free(name);
-}
-
 int create_named_array(char *devnm)
 {
 	int fd;
@@ -129,6 +53,35 @@ int create_named_array(char *devnm)
 	}
 
 	return 1;
+}
+
+char *find_free_devnm(void)
+{
+	static char devnm[MD_NAME_MAX];
+	int devnum;
+
+	for (devnum = 127; devnum != 128; devnum = devnum ? devnum - 1 : 511) {
+		sprintf(devnm, "md%d", devnum);
+
+		if (mddev_busy(devnm))
+			continue;
+
+		if (!conf_name_is_free(devnm))
+			continue;
+
+		if (!udev_is_available()) {
+			/* make sure it is new to /dev too*/
+			dev_t devid = devnm2devid(devnm);
+
+			if (devid && map_dev(major(devid), minor(devid), 0))
+				continue;
+		}
+
+		break;
+	}
+	if (devnum == 128)
+		return NULL;
+	return devnm;
 }
 
 /*
@@ -165,15 +118,13 @@ int create_named_array(char *devnm)
  * When we create devices, we use uid/gid/umask from config file.
  */
 
-int create_mddev(char *dev, char *name, int autof, int trustworthy,
+int create_mddev(char *dev, char *name, int trustworthy,
 		 char *chosen, int block_udev)
 {
 	int mdfd;
 	struct stat stb;
 	int num = -1;
-	int use_mdp = -1;
 	struct createinfo *ci = conf_get_create_info();
-	int parts;
 	char *cname;
 	char devname[37];
 	char devnm[32];
@@ -184,12 +135,6 @@ int create_mddev(char *dev, char *name, int autof, int trustworthy,
 
 	if (chosen == NULL)
 		chosen = cbuf;
-
-	if (autof == 0)
-		autof = ci->autof;
-
-	parts = autof >> 3;
-	autof &= 7;
 
 	strcpy(chosen, DEV_MD_DIR);
 	cname = chosen + strlen(chosen);
@@ -212,12 +157,9 @@ int create_mddev(char *dev, char *name, int autof, int trustworthy,
 					dev, dev+5);
 				return -1;
 			}
-			if (strcmp(cname, "md") == 0)
-				use_mdp = 0;
-			else
-				use_mdp = 1;
+
 			/* recreate name: /dev/md/0 or /dev/md/d0 */
-			sprintf(cname, "%s%d", use_mdp?"d":"", num);
+			sprintf(cname, "%d", num);
 		} else
 			strcpy(cname, dev);
 
@@ -246,10 +188,6 @@ int create_mddev(char *dev, char *name, int autof, int trustworthy,
 				ep = sp;
 			if (ep == sp || *ep || num < 0)
 				num = -1;
-			else if (cname[0] == 'd')
-				use_mdp = 1;
-			else
-				use_mdp = 0;
 		}
 	}
 
@@ -257,18 +195,11 @@ int create_mddev(char *dev, char *name, int autof, int trustworthy,
 	/* named 'METADATA' cannot use 'mdp'. */
 	if (name && name[0] == 0)
 		name = NULL;
-	if (name && trustworthy == METADATA && use_mdp == 1) {
+	if (name && trustworthy == METADATA) {
 		pr_err("%s is not allowed for a %s container. Consider /dev/md%d.\n", dev, name, num);
 		return -1;
 	}
-	if (name && trustworthy == METADATA)
-		use_mdp = 0;
-	if (use_mdp == -1) {
-		if (autof == 4 || autof == 6)
-			use_mdp = 1;
-		else
-			use_mdp = 0;
-	}
+
 	if (num < 0 && trustworthy == LOCAL && name) {
 		/* if name is numeric, possibly prefixed by
 		 * 'md' or '/dev/md', use that for num
@@ -285,7 +216,7 @@ int create_mddev(char *dev, char *name, int autof, int trustworthy,
 		if (ep == n2 || *ep)
 			num = -1;
 		else {
-			sprintf(devnm, "md%s%d", use_mdp ? "_d":"", num);
+			sprintf(devnm, "md%d", num);
 			if (mddev_busy(devnm))
 				num = -1;
 		}
@@ -357,14 +288,15 @@ int create_mddev(char *dev, char *name, int autof, int trustworthy,
 	if (devnm[0] == 0) {
 		if (num < 0) {
 			/* need to choose a free number. */
-			char *_devnm = find_free_devnm(use_mdp);
-			if (_devnm == NULL) {
+			char *_devnm = find_free_devnm();
+
+			if (!_devnm) {
 				pr_err("No avail md devices - aborting\n");
 				return -1;
 			}
 			strcpy(devnm, _devnm);
 		} else {
-			sprintf(devnm, "%s%d", use_mdp?"md_d":"md", num);
+			sprintf(devnm, "md%d", num);
 			if (mddev_busy(devnm)) {
 				pr_err("%s is already in use.\n",
 				       dev);
@@ -415,8 +347,6 @@ int create_mddev(char *dev, char *name, int autof, int trustworthy,
 			}
 			add_dev(devname, &stb, 0, NULL);
 		}
-		if (use_mdp == 1)
-			make_parts(devname, parts);
 
 		if (strcmp(chosen, devname) != 0) {
 			if (mkdir(DEV_NUM_PREF, 0700) == 0) {
@@ -446,8 +376,6 @@ int create_mddev(char *dev, char *name, int autof, int trustworthy,
 			} else if (symlink(devname, chosen) != 0)
 				pr_err("failed to create %s: %s\n",
 					chosen, strerror(errno));
-			if (use_mdp && strcmp(chosen, devname) != 0)
-				make_parts(chosen, parts);
 		}
 	}
 	mdfd = open_dev_excl(devnm);
@@ -498,37 +426,4 @@ int is_mddev(char *dev)
 	}
 
 	return 0;
-}
-
-char *find_free_devnm(int use_partitions)
-{
-	static char devnm[32];
-	int devnum;
-	for (devnum = 127; devnum != 128;
-	     devnum = devnum ? devnum-1 : (1<<9)-1) {
-
-		if (use_partitions)
-			sprintf(devnm, "md_d%d", devnum);
-		else
-			sprintf(devnm, "md%d", devnum);
-		if (mddev_busy(devnm))
-			continue;
-		if (!conf_name_is_free(devnm))
-			continue;
-		if (!udev_is_available()) {
-			/* make sure it is new to /dev too, at least as a
-			 * non-standard */
-			dev_t devid = devnm2devid(devnm);
-			if (devid) {
-				char *dn = map_dev(major(devid),
-						   minor(devid), 0);
-				if (dn && ! is_standard(dn, NULL))
-					continue;
-			}
-		}
-		break;
-	}
-	if (devnum == 128)
-		return NULL;
-	return devnm;
 }

Assemble.c
@@ -1573,8 +1573,7 @@ try_again:
 			/* Ignore 'host:' prefix of name */
 			name = strchr(name, ':')+1;
 
-		mdfd = create_mddev(mddev, name, ident->autof, trustworthy,
-				    chosen_name, 0);
+		mdfd = create_mddev(mddev, name, trustworthy, chosen_name, 0);
 	}
 	if (mdfd < 0) {
 		st->ss->free_super(st);

Build.c
@@ -75,8 +75,7 @@ int Build(struct mddev_ident *ident, struct mddev_dev *devlist, struct shape *s,
 
 	/* We need to create the device.  It can have no name. */
 	map_lock(&map);
-	mdfd = create_mddev(ident->devname, NULL, c->autof, LOCAL,
-			    chosen_name, 0);
+	mdfd = create_mddev(ident->devname, NULL, LOCAL, chosen_name, 0);
 	if (mdfd < 0) {
 		map_unlock(&map);
 		return 1;

Create.c
@@ -1045,7 +1045,7 @@ int Create(struct supertype *st, struct mddev_ident *ident, int subdevs,
 
 	/* We need to create the device */
 	map_lock(&map);
-	mdfd = create_mddev(ident->devname, ident->name, c->autof, LOCAL, chosen_name, 1);
+	mdfd = create_mddev(ident->devname, ident->name, LOCAL, chosen_name, 1);
 	if (mdfd < 0) {
 		map_unlock(&map);
 		return 1;

Incremental.c
@@ -107,8 +107,6 @@ int Incremental(struct mddev_dev *devlist, struct context *c,
 	char *devname = devlist->devname;
 	int journal_device_missing = 0;
 
-	struct createinfo *ci = conf_get_create_info();
-
 	if (!stat_is_blkdev(devname, &rdev))
 		return rv;
 	dfd = dev_open(devname, O_RDONLY);
@@ -234,16 +232,6 @@ int Incremental(struct mddev_dev *devlist, struct context *c,
 	if (trustworthy == LOCAL_ANY)
 		trustworthy = LOCAL;
 
-	/* There are three possible sources for 'autof':  command line,
-	 * ARRAY line in mdadm.conf, or CREATE line in mdadm.conf.
-	 * ARRAY takes precedence, then command line, then
-	 * CREATE.
-	 */
-	if (match && match->autof)
-		c->autof = match->autof;
-	if (c->autof == 0)
-		c->autof = ci->autof;
-
 	name_to_use = info.name;
 	if (name_to_use[0] == 0 && is_container(info.array.level)) {
 		name_to_use = info.text_version;
@@ -297,8 +285,8 @@ int Incremental(struct mddev_dev *devlist, struct context *c,
 			goto out;
 
 		/* Couldn't find an existing array, maybe make a new one */
-		mdfd = create_mddev(match ? match->devname : NULL,
-				    name_to_use, c->autof, trustworthy, chosen_name, 0);
+		mdfd = create_mddev(match ? match->devname : NULL, name_to_use, trustworthy,
+				    chosen_name, 0);
 
 		if (mdfd < 0)
 			goto out_unlock;
@@ -1607,10 +1595,7 @@ static int Incremental_container(struct supertype *st, char *devname,
 			if (match)
 				trustworthy = LOCAL;
 
-			mdfd = create_mddev(match ? match->devname : NULL,
-					    ra->name,
-					    c->autof,
-					    trustworthy,
+			mdfd = create_mddev(match ? match->devname : NULL, ra->name, trustworthy,
 					    chosen_name, 0);
 
 			if (!is_fd_valid(mdfd)) {

ReadMe.c
@@ -132,7 +132,7 @@ struct option long_options[] = {
 	{"spare-disks", 1, 0, 'x'},
 	{"spare-devices", 1, 0, 'x'},
 	{"size", 1, 0, 'z'},
-	{"auto", 1, 0, Auto},
+	{"auto", 1, 0, Auto}, /* Deprecated, left for backward compatibility */
 	{"assume-clean", 0, 0, AssumeClean },
 	{"write-zeroes", 0, 0, WriteZeroes },
 	{"metadata", 1, 0, 'e'}, /* superblock format */
@@ -578,7 +578,7 @@ char Help_config[] =
 "\n"
 " Other configuration lines include:\n"
 "  mailaddr, mailfrom, program, monitordelay    used for --monitor mode\n"
-"  create, auto                                 used when creating device names in /dev\n"
+"  create,                                      used when creating device names in /dev\n"
 "  homehost, homecluster, policy, part-policy   used to guide policy in various\n"
 "                                               situations\n"
 "\n"

config.c
@@ -171,7 +171,6 @@ inline void ident_init(struct mddev_ident *ident)
 	assert(ident);
 
 	ident->assembled = false;
-	ident->autof = 0;
 	ident->bitmap_fd = -1;
 	ident->bitmap_file = NULL;
 	ident->container = NULL;
@@ -397,7 +396,6 @@ struct mddev_dev *load_containers(void)
 }
 
 struct createinfo createinfo = {
-	.autof = 2, /* by default, create devices with standard names */
 	.names = 0, /* By default, stick with numbered md devices. */
 	.bblist = 1, /* Use a bad block list by default */
 #ifdef DEBIAN
@@ -408,52 +406,6 @@ struct createinfo createinfo = {
 #endif
 };
 
-int parse_auto(char *str, char *msg, int config)
-{
-	int autof;
-	if (str == NULL || *str == 0)
-		autof = 2;
-	else if (strcasecmp(str, "no") == 0)
-		autof = 1;
-	else if (strcasecmp(str, "yes") == 0)
-		autof = 2;
-	else if (strcasecmp(str, "md") == 0)
-		autof = config ? 5:3;
-	else {
-		/* There might be digits, and maybe a hypen, at the end */
-		char *e = str + strlen(str);
-		int num = 4;
-		int len;
-		while (e > str && isdigit(e[-1]))
-			e--;
-		if (*e) {
-			num = atoi(e);
-			if (num <= 0)
-				num = 1;
-		}
-		if (e > str && e[-1] == '-')
-			e--;
-		len = e - str;
-		if ((len == 2 && strncasecmp(str, "md", 2) == 0)) {
-			autof = config ? 5 : 3;
-		} else if ((len == 3 && strncasecmp(str, "yes", 3) == 0)) {
-			autof = 2;
-		} else if ((len == 3 && strncasecmp(str, "mdp", 3) == 0)) {
-			autof = config ? 6 : 4;
-		} else if ((len == 1 && strncasecmp(str, "p", 1) == 0) ||
-			   (len >= 4 && strncasecmp(str, "part", 4) == 0)) {
-			autof = 6;
-		} else {
-			pr_err("%s arg of \"%s\" unrecognised: use no,yes,md,mdp,part\n"
-				"        optionally followed by a number.\n",
-				msg, str);
-			exit(2);
-		}
-		autof |= num << 3;
-	}
-	return autof;
-}
-
 static void createline(char *line)
 {
 	char *w;
@@ -461,7 +413,8 @@ static void createline(char *line)
 
 	for (w = dl_next(line); w != line; w = dl_next(w)) {
 		if (strncasecmp(w, "auto=", 5) == 0)
-			createinfo.autof = parse_auto(w + 5, "auto=", 1);
+			/* auto is no supported now, ignore it silently */
+			continue;
 		else if (strncasecmp(w, "owner=", 6) == 0) {
 			if (w[6] == 0) {
 				pr_err("missing owner name\n");
@@ -630,9 +583,9 @@ void arrayline(char *line)
 			if (!mis.st)
 				pr_err("metadata format %s unknown, ignored.\n",
 				       w + 9);
-		} else if (strncasecmp(w, "auto=", 5) == 0 ) {
-			/* whether to create device special files as needed */
-			mis.autof = parse_auto(w + 5, "auto type", 0);
+		} else if (strncasecmp(w, "auto=", 5) == 0) {
+			/* Ignore for backward compatibility */
+			continue;
 		} else if (strncasecmp(w, "member=", 7) == 0) {
 			/* subarray within a container */
 			mis.member = xstrdup(w + 7);

mdadm.8.in
@@ -969,63 +969,6 @@ array, and no resync, recovery, or reshape will be started. It works with
 Create, Assemble, Manage and Misc mode.
 
 .TP
-.BR \-a ", " "\-\-auto{=yes,md,mdp,part,p}{NN}"
-Instruct mdadm how to create the device file if needed, possibly allocating
-an unused minor number.  "md" causes a non-partitionable array
-to be used (though since Linux 2.6.28, these array devices are in fact
-partitionable).  "mdp", "part" or "p" causes a partitionable array (2.6 and
-later) to be used.  "yes" requires the named md device to have
-a 'standard' format, and the type and minor number will be determined
-from this.  With mdadm 3.0, device creation is normally left up to
-.I udev
-so this option is unlikely to be needed.
-See DEVICE NAMES below.
-
-The argument can also come immediately after
-"\-a".  e.g. "\-ap".
-
-If
-.B \-\-auto
-is not given on the command line or in the config file, then
-the default will be
-.BR \-\-auto=yes .
-
-If
-.B \-\-scan
-is also given, then any
-.I auto=
-entries in the config file will override the
-.B \-\-auto
-instruction given on the command line.
-
-For partitionable arrays,
-.I mdadm
-will create the device file for the whole array and for the first 4
-partitions.  A different number of partitions can be specified at the
-end of this option (e.g.
-.BR \-\-auto=p7 ).
-If the device name ends with a digit, the partition names add a 'p',
-and a number, e.g.
-.IR /dev/md/home1p3 .
-If there is no trailing digit, then the partition names just have a
-number added, e.g.
-.IR /dev/md/scratch3 .
-
-If the md device name is in a 'standard' format as described in DEVICE
-NAMES, then it will be created, if necessary, with the appropriate
-device number based on that name.  If the device name is not in one of these
-formats, then an unused device number will be allocated.  The device
-number will be considered unused if there is no active array for that
-number, and there is no entry in /dev for that number and with a
-non-standard name.  Names that are not in 'standard' format are only
-allowed in "/dev/md/".
-
-This is meaningful with
-.B \-\-create
-or
-.BR \-\-build .
-
-.TP
 .BR \-a ", " "\-\-add"
 This option can be used in Grow mode in two cases.
 
@@ -1167,10 +1110,6 @@ are present.  This is only needed with
 .B \-\-scan,
 and can be used if the physical connections to devices are
 not as reliable as you would like.
-
-.TP
-.BR \-a ", " "\-\-auto{=no,yes,md,mdp,part}"
-See this option under Create and Build options.
 
 .TP
 .BR \-b ", " \-\-bitmap=

commit 2e308365c669448cf31f18a84b3818c890344d10

mdopen: remove wrong condition
After mentioned patch, this condition get opposite meaning and it
is blocking creation in cases where it was supported.

Remove it now.

Fixes: 119cdcad049e ("mdadm: drop auto= support")
Signed-off-by: Mariusz Tkaczyk <mariusz.tkaczyk@linux.intel.com>

mdopen.c
@@ -192,13 +192,8 @@ int create_mddev(char *dev, char *name, int trustworthy,
 	}
 
 	/* Now determine device number */
-	/* named 'METADATA' cannot use 'mdp'. */
 	if (name && name[0] == 0)
 		name = NULL;
-	if (name && trustworthy == METADATA) {
-		pr_err("%s is not allowed for a %s container. Consider /dev/md%d.\n", dev, name, num);
-		return -1;
-	}
 
 	if (num < 0 && trustworthy == LOCAL && name) {
 		/* if name is numeric, possibly prefixed by