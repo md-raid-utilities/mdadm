e572b836b8edf6e7a377b534944a6e5d3d89ffcf
Mariusz Tkaczyk <mariusz.tkaczyk@linux.intel.com>
2024-11-29 09:48:03 +0100

    test: Fix saving artifacts
    Currently, if error is returned by test command, execution of other
    steps is aborted. In that case, continue-on-error safe artifact but
    return error later and fail the job.
    
    If executions passed, there are no artifacts to safe, therefore do not
    safe them.
    
    Signed-off-by: Mariusz Tkaczyk <mariusz.tkaczyk@linux.intel.com>

--- .github/tools/run_mdadm_tests.sh
+++ .github/tools/run_mdadm_tests.sh
@@ -5,7 +5,14 @@ sudo make -j$(nproc)
 sudo make install
 sudo mdadm -Ss
 sudo ./test setup
+
+# Uncomment and adjust this to minimalize testing time for CI or test improvements.
+# --tests=test1,test2,...     Comma separated list of tests to run
+
+#sudo ./test --tests=00createnames
+
 sudo ./test --skip-broken --no-error --disable-integrity --disable-multipath --disable-linear --keep-going
+
 ret=$?
 sudo ./test cleanup
 exit $ret
