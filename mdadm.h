md-raid-utilities/mdadm

commit d354d314db86379f18a4ccd35af9f6e56635b61d

mdadm: Create array with sync del gendisk mode
kernel patch 9e59d609763f ('md: call del_gendisk in control path') calls
del_gendisk in sync way. After the patch mentioned just now, device node
(/dev/md0 .e.g) will disappear after mdadm --stop command. It resolves the
problem raid can be created again because raid can be created when opening
device node. Then regression tests will be interrupted.

But it causes an error when assembling array which has been fixed by pr182.
So people can't assemble array if they use new kernel and old mdadm. So
in kernel space, 25db5f284fb8 ('md: add legacy_async_del_gendisk mod') is
used to fix this problem. The default is async mode.

async del mode will be removed in future. We'll start use sync del mode in
new mdadm version. So people will not see failure when upgrading to the
new mdadm version with sync del mode.

Signed-off-by: Xiao Ni <xni@redhat.com>

mdadm.h
@@ -141,6 +141,8 @@ struct dlm_lksb {
 #define MDMON_DIR "/run/mdadm"
 #endif /* MDMON_DIR */
 
+#define MD_MOD_ASYNC_DEL_GENDISK "legacy_async_del_gendisk"
+
 /* FAILED_SLOTS is where to save files storing recent removal of array
  * member in order to allow future reuse of disk inserted in the same
  * slot for array recovery
@@ -855,6 +857,7 @@ extern int restore_stripes(int *dest, unsigned long long *offsets,
 			   unsigned long long start, unsigned long long length,
 			   char *src_buf);
 extern bool sysfs_is_libata_allow_tpm_enabled(const int verbose);
+extern bool init_md_mod_param(void);
 
 #ifndef Sendmail
 #define Sendmail "/usr/lib/sendmail -t"

mdopen.c
@@ -148,6 +148,11 @@ int create_mddev(char *dev, char *name, int trustworthy,
 	char devnm[32];
 	char cbuf[400];
 
+	if (!init_md_mod_param()) {
+		pr_err("init md module parameters fail\n");
+		return -1;
+	}
+
 	if (!udev_is_available())
 		block_udev = 0;
 

util.c
@@ -2559,3 +2559,48 @@ bool is_file(const char *path)
 
 	return true;
 }
+
+bool set_md_mod_parameter(const char *name, const char *value)
+{
+	char path[256];
+	int fd;
+	bool ret = true;
+
+	snprintf(path, sizeof(path), "/sys/module/md_mod/parameters/%s", name);
+
+	fd = open(path, O_WRONLY);
+	if (fd < 0) {
+		pr_err("Can't open %s\n", path);
+		return false;
+	}
+
+	if (write(fd, value, strlen(value)) != (ssize_t)strlen(value)) {
+		pr_err("Failed to write to %s\n", path);
+		ret = false;
+	}
+
+	close(fd);
+	return ret;
+}
+
+/* Init kernel md_mod parameters here if needed */
+bool init_md_mod_param(void)
+{
+	bool ret = true;
+
+	/*
+	 * In kernel 9e59d609763f calls del_gendisk in sync way. So device
+	 * node can be removed after stop command. But it can introduce a
+	 * regression which can be fixed by github pr182. New mdadm version
+	 * with pr182 can work well with new kernel. But users who don't
+	 * update mdadm and update to new kernel, they can't assemble array
+	 * anymore. So kernel adds a kernel parameter legacy_async_del_gendisk
+	 * and uses async as default.
+	 * We'll use sync mode since 6.18 rather than async mode. So in future
+	 * the kernel parameter will be removed.
+	 */
+	if (get_linux_version() >= 6018000)
+		ret = set_md_mod_parameter(MD_MOD_ASYNC_DEL_GENDISK, "N");
+
+	return ret;
+}

commit b74f7ad5aff2b0517ade68d3e4f45348099b6374

mdadm: load md_mod first
Load md_mod first before setting module parameter legacy_async_del_gendisk
Everything works well if md_mod is built in kernel. If not, create and
assemble will fail.

Fixes: d354d314db86 ("mdadm: Create array with sync del gendisk mode")
Signed-off-by: Xiao Ni <xni@redhat.com>

mdadm.h
@@ -860,7 +860,7 @@ extern int restore_stripes(int *dest, unsigned long long *offsets,
 			   unsigned long long start, unsigned long long length,
 			   char *src_buf);
 extern bool sysfs_is_libata_allow_tpm_enabled(const int verbose);
-extern bool init_md_mod_param(void);
+extern bool init_md_mod(void);
 
 #ifndef Sendmail
 #define Sendmail "/usr/lib/sendmail -t"

mdopen.c
@@ -38,33 +38,15 @@ int create_named_array(char *devnm)
 	};
 
 	fd = open(new_array_file, O_WRONLY);
-	if (fd < 0 && errno == ENOENT) {
-		char buf[PATH_MAX] = {0};
-		char *env_ptr;
-
-		env_ptr = getenv("PATH");
-		/*
-		 * When called by udev worker context, path of modprobe
-		 * might not be in env PATH. Set sbin paths into PATH
-		 * env to avoid potential failure when run modprobe here.
-		 */
-		if (env_ptr)
-			snprintf(buf, PATH_MAX - 1, "%s:%s", env_ptr,
-				 "/sbin:/usr/sbin:/usr/local/sbin");
-		else
-			snprintf(buf, PATH_MAX - 1, "%s",
-				 "/sbin:/usr/sbin:/usr/local/sbin");
-
-		setenv("PATH", buf, 1);
-
-		if (system("modprobe md_mod") == 0)
-			fd = open(new_array_file, O_WRONLY);
-	}
 	if (fd >= 0) {
 		n = write(fd, devnm, strlen(devnm));
 		close(fd);
+	} else {
+		pr_err("Fail to open %s\n", new_array_file);
+		return 0;
 	}
-	if (fd < 0 || n != (int)strlen(devnm)) {
+
+	if (n != (int)strlen(devnm)) {
 		pr_err("Fail to create %s when using %s, fallback to creation via node\n",
 			devnm, new_array_file);
 		return 0;
@@ -148,7 +130,7 @@ int create_mddev(char *dev, char *name, int trustworthy,
 	char devnm[32];
 	char cbuf[400];
 
-	if (!init_md_mod_param()) {
+	if (!init_md_mod()) {
 		pr_err("init md module parameters fail\n");
 		return -1;
 	}

util.c
@@ -2583,10 +2583,41 @@ bool set_md_mod_parameter(const char *name, const char *value)
 	return ret;
 }
 
-/* Init kernel md_mod parameters here if needed */
-bool init_md_mod_param(void)
+/* Init kernel md_mod and parameters here if needed */
+bool init_md_mod(void)
 {
 	bool ret = true;
+	char module_path[32];
+	FILE *fp;
+
+	snprintf(module_path, sizeof(module_path), "/sys/module/md_mod");
+	fp = fopen(module_path, "r");
+	if (fp == NULL) {
+
+		char buf[PATH_MAX] = {0};
+		char *env_ptr;
+
+		env_ptr = getenv("PATH");
+		/*
+		 * When called by udev worker context, path of modprobe
+		 * might not be in env PATH. Set sbin paths into PATH
+		 * env to avoid potential failure when run modprobe here.
+		 */
+		if (env_ptr)
+			snprintf(buf, PATH_MAX - 1, "%s:%s", env_ptr,
+				 "/sbin:/usr/sbin:/usr/local/sbin");
+		else
+			snprintf(buf, PATH_MAX - 1, "%s",
+				 "/sbin:/usr/sbin:/usr/local/sbin");
+
+		setenv("PATH", buf, 1);
+
+		if (system("modprobe md_mod") != 0) {
+			pr_err("Can't load kernel module md_mod\n");
+			return false;
+		}
+	} else
+		fclose(fp);
 
 	/*
 	 * In kernel 9e59d609763f calls del_gendisk in sync way. So device