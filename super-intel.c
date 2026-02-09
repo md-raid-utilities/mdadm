md-raid-utilities/mdadm

commit 127e38b59cbdf717d1569bcdc75b8d823d8485f3

imsm: Fix RAID0 to RAID10 migration
Support for RAID10 with +4 disks in IMSM introduced an inconsistency
between the VROC UEFI driver and Linux IMSM. VROC UEFI does not
support RAID10 with +4 disks, therefore appropriate protections were
added to the mdadm IMSM code that results in skipping processing of
such RAID in the UEFI phase. Unfortunately the case of migration
RAID0 2 disks to RAID10 4 disks was omitted, this case requires
maintaining compatibility with the VROC UEFI driver because it is
supported.

For RAID10 +4 disk the MPB_ATTRIB_RAID10_EXT attribute is set in the
metadata, thanks to which the UEFI driver does not process such RAID.
In the series adding support, a new metadata raid level value
IMSM_T_RAID10 was also introduced. It is not recognized by VROC UEFI.

The issue is caused by the fact that in the case of the mentioned
migration, IMSM_T_RAID10 is entered into the metadata but attribute
MPB_ATTRIB_RAID10_EXT is not entered, which causes an attempt to
process such RAID in the UEFI phase. This situation results in
the platform hang during booting in UEFI phase, this also results in
data loss after failed and interrupted RAID processing in VROC UEFI.

The above situation is result of the update_imsm_raid_level()
function, for the mentioned migration function is executed on a map
with a not yet updated number of disks.

The fix is to explicitly handle migration in the function mentioned
above to maintain compatibility with VROC UEFI driver.

Steps to reproduce:
mdadm -C /dev/md/imsm0 -e imsm -n 2 /dev/nvme[1,2]n1 -R
mdadm -C /dev/md/vol -l 0 -n 2 /dev/nvme[1,2]n1 --assume-clean -R
mdadm -a /dev/md127 /dev/nvme3n1
mdadm -a /dev/md127 /dev/nvme4n1
mdadm -G /dev/md126 -l 10
reboot

Fixes: 27550b13297a ("imsm: add support for literal RAID 10")
Signed-off-by: Blazej Kucman <blazej.kucman@intel.com>

super-intel.c
@@ -1327,6 +1327,19 @@ static void update_imsm_raid_level(struct imsm_map *map, int new_level)
 		return;
 	}
 
+	/*
+	 * RAID0 to RAID10 migration.
+	 * Due to the compatibility with VROC UEFI must be maintained, this case must be handled
+	 * separately, because the map does not have an updated number of disks.
+	 */
+	if (map->raid_level == IMSM_T_RAID0) {
+		if (map->num_members == 2)
+			map->raid_level = IMSM_T_RAID1;
+		else
+			map->raid_level = IMSM_T_RAID10;
+		return;
+	}
+
 	if (map->num_members == 4) {
 		if (map->raid_level == IMSM_T_RAID10 || map->raid_level == IMSM_T_RAID1)
 			return;

commit fa667c69dcb95ac128bbf90468b42b05ccdee8ac

imsm: Fix UEFI backward compatibility for RAID10D4
The referenced commit introduces an incorrect RAID level set for RAID10 with 4 drives, which must
remain backwards compatibility with UEFI VROC driver. For such RAID, VROC UEFI requires
the MPB_ATTRIB_RAID1 level attribute in metadata and RAID1 in map. However, mentioned change cause
writes RAID10 instead, which VROC UEFI cannot handle correctly.

As a result, RAID10 4 disks is no longer recognized by VROC UEFI since version 9.3. On earlier
versions the incorrect metadata may even cause a platform hang during the UEFI boot phase.

The update_imsm_raid_level() function handles both creation and migration flows. During RAID
creation, the function receives an initial map where the `level` variable is set to 0. This causes
the code path responsible for the R0 -> R10 migration to run.

To prevent the above behavior, a new define IMSM_T_LEVEL_UNKNOWN is introduced and used to
initialize the `level` variable in map during volume creation, ensuring that the migration path
is not entered.

Steps to reproduce:
mdadm -C /dev/md/imsm0 -e imsm -n 4 /dev/nvme[1,2,3,4]n1 -R
mdadm -C /dev/md/vol -l 10 4 /dev/nvme[1,2,3,4]n1 --assume-clean -R
reboot

Fixes: 127e38b59cbd ("imsm: Fix RAID0 to RAID10 migration")
Signed-off-by: Blazej Kucman <blazej.kucman@graidtech.com>

super-intel.c
@@ -181,6 +181,7 @@ struct imsm_map {
 #define IMSM_T_RAID1 1
 #define IMSM_T_RAID5 5
 #define IMSM_T_RAID10 10
+#define IMSM_T_LEVEL_UNKNOWN 255
 	__u8  num_members;	/* number of member disks */
 	__u8  num_domains;	/* number of parity domains */
 	__u8  failed_disk_num;  /* valid only when state is degraded */
@@ -1326,7 +1327,6 @@ static void update_imsm_raid_level(struct imsm_map *map, int new_level)
 		map->raid_level = new_level;
 		return;
 	}
-
 	/*
 	 * RAID0 to RAID10 migration.
 	 * Due to the compatibility with VROC UEFI must be maintained, this case must be handled
@@ -1343,11 +1343,9 @@ static void update_imsm_raid_level(struct imsm_map *map, int new_level)
 	if (map->num_members == 4) {
 		if (map->raid_level == IMSM_T_RAID10 || map->raid_level == IMSM_T_RAID1)
 			return;
-
 		map->raid_level = IMSM_T_RAID1;
 		return;
 	}
-
 	map->raid_level = IMSM_T_RAID10;
 }
 
@@ -5792,6 +5790,7 @@ static int init_super_imsm_volume(struct supertype *st, mdu_array_info_t *info,
 	}
 	map->num_members = info->raid_disks;
 
+	map->raid_level = IMSM_T_LEVEL_UNKNOWN;
 	update_imsm_raid_level(map, info->level);
 	set_num_domains(map);
 