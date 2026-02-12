5f8872e64fa560e641ff454a815a34dc5d0c29a9
Martin Wilck <mwilck@suse.com>
2026-02-12 21:34:49 +0100

    mdadm.conf: add "PROBING ddf_extended" option
    Add a configuration line PROBING to mdadm.conf. If the parameter
    "ddf_extended" is set on this line, use the extended DDF header search
    introduced by commit f2197b6b6c14 ("super-ddf: optimize DDF header search
    for widely used RAID controllers"), at the cost of slower probing.
    Otherwise, just check for the header in the last sector of the disk,
    as usual.
    
    Signed-off-by: Martin Wilck <mwilck@suse.com>

--- mdadm.h
+++ mdadm.h
@@ -1639,6 +1639,7 @@ extern char *conf_get_homehost(int *require_homehostp);
 extern char *conf_get_homecluster(void);
 extern int conf_get_monitor_delay(void);
 extern bool conf_get_sata_opal_encryption_no_verify(void);
+extern bool conf_get_probing_ddf_extended(void);
 extern char *conf_line(FILE *file);
 extern char *conf_word(FILE *file, int allow_key);
 extern void print_quoted(char *str);
