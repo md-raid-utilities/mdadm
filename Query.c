md-raid-utilities/mdadm

commit bcc3ab1728da61e5519e1f01597c8da0c5bc769b

mdadm: Query.c fix coverity issues
Fixing the following coding errors the coverity tools found:

* Event leaked_storage: Variable "sra" going out of scope leaks the
storage it points to.

* Event uninit_use_in_call: Using uninitialized value "larray_size" when
calling "human_size_brief".

Signed-off-by: Nigel Croxon <ncroxon@redhat.com>

Query.c
@@ -39,7 +39,7 @@ int Query(char *dev)
 	struct mdinfo info;
 	struct mdinfo *sra;
 	struct supertype *st = NULL;
-	unsigned long long larray_size;
+	unsigned long long larray_size = 0;
 	struct stat stb;
 	char *mddev;
 	mdu_disk_info_t disc;
@@ -136,5 +136,7 @@ int Query(char *dev)
 		if (st->ss == &super0)
 			put_md_name(mddev);
 	}
+	free(sra);
+
 	return 0;
 }

commit e444993a8a9094640bf58ba6f61734131d690ada

mdadm: Query.c use sysfs_free() for proper resource cleanup
free(sra) against mdinfo object allocated by sysfs_read() in Query.c
can cause improper cleanup before return. This change uses sysfs_free()
instead which should do proper cleanup of the object.

Fixes: bcc3ab1728da ("mdadm: Query.c fix coverity issues")

Signed-off-by: Satyam Sinha <satsinha@amazon.com>

Query.c
@@ -134,7 +134,7 @@ int Query(char *dev)
 		if (st->ss == &super0)
 			put_md_name(mddev);
 	}
-	free(sra);
+	sysfs_free(sra);
 
 	return 0;
 }